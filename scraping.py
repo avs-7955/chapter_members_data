52e07818f4dca4f22c0f283221a7e29ca9e80561
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-12-09 09:24:29 +0530

    Add comments

--- scraping.py
+++ scraping.py
@@ -4,21 +4,25 @@ from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 
+# Website containing data about the Chapter Members
 website = 'https://bni-india.in/en-IN/chapterdetail?chapterId=P%2BIWaeuOSfF3Mxm03FLSzA%3D%3D&name=BNI+BNI+Alpha'
 
+# Chrome Driver path
 driver = webdriver.Chrome("D:\VIT\Self_Dev\CODING\Web Scraping\chromedriver")
 
 driver.get(website)
-time.sleep(5)
+time.sleep(5)  # Wating for the website to be fetched
+
+# Clicking the all members btn
 members_btn = driver.find_element(By.XPATH, '//a[@class="numberLink"]')
-# members_btn.click()
 driver.execute_script("arguments[0].click();", members_btn)
 
 page_source = driver.page_source
 
+# Integrating BeautifulSoup
 soup = BeautifulSoup(page_source, 'html.parser')
 
-
+# Creating empty list for the data
 name = []
 company = []
 prof = []
@@ -27,7 +31,9 @@ profile_url = []
 
 
 def extract_data():
+    # Finding the rows of the table
     members_list = soup.find_all('tr', attrs={'role': 'row'})
+    # Extracing the information and appending it to the lists
     for member in members_list:
         text = member.text.strip()
         arr = text.split('\n')
@@ -35,10 +41,12 @@ def extract_data():
             name.append(arr[0])
             company.append(arr[1])
             prof.append(arr[2])
+            # If the phone number isn't available, adding blank string
             if(len(arr) <= 3):
                 phone.append(' ')
             else:
                 phone.append(arr[3])
+    # Extracing profile URL of all the members
     all_links = soup.findAll('a', class_='linkone')
     for link in all_links:
         if(len(link['href']) != 0):
@@ -49,29 +57,29 @@ def extract_data():
 
 extract_data()
 time.sleep(5)
+
+# Clicking the button to get the next page elements
 click_nxt_btn = driver.find_element(
     By.XPATH, '//li[@class="paginate_button next"]')
-# click_nxt_btn.click()
 driver.execute_script("arguments[0].click();", click_nxt_btn)
 
+# Integrating BeautifulSoup and extracting data
 page_source = driver.page_source
-
-
 soup = BeautifulSoup(page_source, 'html.parser')
 extract_data()
 time.sleep(5)
+
+# Clicking the button to get the next page elements
 click_nxt_btn = driver.find_element(
     By.XPATH, '//li[@class="paginate_button next"]')
-# click_nxt_btn.click()
 driver.execute_script("arguments[0].click();", click_nxt_btn)
 
+# Integrating BeautifulSoup and extracting data
 page_source = driver.page_source
-
-
 soup = BeautifulSoup(page_source, 'html.parser')
 extract_data()
 
-
+# Creating a data frame and stroring the data into excel file
 df = pd.DataFrame({'MemberName': name, 'Company': company,
                    'Profession': prof, 'MobileNo.': phone, 'ProfileURL': profile_url})
 df.to_excel('members_list.xlsx', index=False)
